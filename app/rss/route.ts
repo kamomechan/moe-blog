e30e197ee3709378401b3c73e828b33a013c470b
kamomechan <texiachann.@gmail.com>
2025-11-13 15:08:54 +0800

    Add rss page

---
+++ app/rss/route.ts
@@ -0,0 +1,27 @@
+import preview from "@/app/lib/preview";
+
+export function GET() {
+  const title = "moe-blog";
+  const description =
+    "Discussing visual novels and moe culture, with occasional posts on open-source projects.";
+  const link = "https://tia-chan.top";
+  const rss = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+<channel>
+ <title>${title}</title>
+ <description>${description}</description>
+ <link>${link}</link>
+ ${preview.map(({ data: item }) => {
+   return `<item>
+    <title>${item.title}</title>
+    <description>${item.description}</description>
+    <link>${link}${item.href}</link>
+    <pubDate>${item.date}</pubDate>
+    <guid isPermaLink="false">https://tia-chan.top${item.href}</guid>
+ </item>`;
+ })}
+</channel>
+</rss>`;
+  const headers = new Headers({ "content-type": "application/xml" });
+  return new Response(rss, { headers });
+}
