5017053f2808bf84fe2437a513aaeb0d19e98ea1
nokutan <h4towt6cd@mozmail.com>
2025-11-26 19:48:13 +0800

    Change the initial rendering method of the comment section

--- app/ui/post/comments.tsx
+++ app/ui/post/comments.tsx
@@ -1,13 +1,15 @@
 "use client";
 import Form from "./create-form";
 import CommentsList from "./comments-list";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Comments(props: { id: string }) {
   const [comments, setComments] = useState(null);
   const [isSuccess, setIsSuccess] = useState(false);
+  const [isObserver, setIsObserver] = useState(false);
+  const commentsRef = useRef<HTMLDivElement>(null);
 
-  // Fetch comments only on first visit and when an update occurs.
+  // Comments are fetched only when the user first visits the comment area or when an update is triggered.
   useEffect(() => {
     const fetchComments = async () => {
       const response = await fetch(`/api/data?postId=${props.id}`);
@@ -19,20 +21,49 @@ export default function Comments(props: { id: string }) {
       const data = JSON.parse(comments);
       setComments(data);
     };
-
-    fetchComments();
+    if (isObserver) {
+      fetchComments();
+    }
   }, [isSuccess]);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsObserver(true);
+          setIsSuccess(!isSuccess);
+        }
+      });
+    });
+    const commentsElement = commentsRef.current;
+    if (!commentsElement) return;
+    observer.observe(commentsElement);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <div className="w-[89vw] m-[6.5vw_auto_13vw] p-[7.8vw_6.5vw_5.2vw] rounded-[1.3vw] shadow-[0_2.6041666667vw_2.6041666667vw_rgba(71,80,104,.14)] lg:w-[50vw] lg:m-[3vw_auto_5vw] lg:p-[3.1vw_4vw_2vw] lg:shadow-[0_1.0416666667vw_1.0416666667vw_rgba(71,80,104,.14)] lg:rounded-[0.5vw] dark:shadow-[0_2.6041666667vw_3.6041666667vw_rgba(71,80,104,.34)]">
-      <div className="text-[#3a7aae] text-2xl font-semibold mb-[9vw] lg:mb-[3vw] dark:text-[#4586bb]">
-        Comments
-      </div>
-      <Form
-        postId={props.id}
-        setIsSuccess={setIsSuccess}
-        isSuccess={isSuccess}
-      />
+    <div
+      className="w-[89vw] m-[6.5vw_auto_13vw] p-[7.8vw_6.5vw_5.2vw] rounded-[1.3vw] shadow-[0_2.6041666667vw_2.6041666667vw_rgba(71,80,104,.14)] lg:w-[50vw] lg:m-[3vw_auto_5vw] lg:p-[3.1vw_4vw_2vw] lg:shadow-[0_1.0416666667vw_1.0416666667vw_rgba(71,80,104,.14)] lg:rounded-[0.5vw] dark:shadow-[0_2.6041666667vw_3.6041666667vw_rgba(71,80,104,.34)]"
+      ref={commentsRef}
+    >
+      {/* Prevents basic bots that do not simulate scrolling. */}
+      {isObserver && (
+        <>
+          <div className="text-[#3a7aae] text-2xl font-semibold mb-[9vw] lg:mb-[3vw] dark:text-[#4586bb]">
+            Comments
+          </div>
+
+          <Form
+            postId={props.id}
+            setIsSuccess={setIsSuccess}
+            isSuccess={isSuccess}
+          />
+        </>
+      )}
+
       <CommentsList
         comments={comments}
         setIsSuccess={setIsSuccess}
